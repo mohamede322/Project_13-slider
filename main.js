59c791c5b5db67b80bcde2c1e58917759b30feb7
Psycho <93525954+itspsycholol322@users.noreply.github.com>
2021-12-25 11:42:46 +0200

    intial commit - project complete

---
+++ main.js
@@ -0,0 +1,55 @@
+let imagesContainer = document.querySelector(".images")
+let images = document.querySelectorAll("img")
+let leftArrow = document.getElementById("left-arrow")
+let rightArrow = document.getElementById("right-arrow")
+
+let sliding =  setInterval(slide, 5000)
+
+click()
+
+let counter = 0
+
+function slide() {
+    images.forEach((img) => {
+        width =  img.clientWidth
+    })
+    counter++
+    if (counter === images.length) {
+        counter = 0
+    }
+    imagesContainer.style.transform = `translateX(-${width * counter}px)`
+    console.log(counter)
+}
+
+function click() {
+    leftArrow.addEventListener("click" , () => {
+        images.forEach((img) => {
+            width =  img.clientWidth
+        })
+        counter--
+        if (counter === -1) {
+            counter = images.length - 1
+            imagesContainer.style.transition = "none"
+        }else {
+            imagesContainer.style.transition = "0.5s ease-in-out"
+        }
+        imagesContainer.style.transform = `translateX(-${width * counter}px)`
+        clearInterval(sliding)
+        sliding = setInterval(slide, 5000)
+    })
+    rightArrow.addEventListener("click" , () => {
+        images.forEach((img) => {
+            width =  img.clientWidth
+        })
+        counter++
+        if (counter === images.length) {
+            counter = 0
+            imagesContainer.style.transition = "none"
+        }else {
+            imagesContainer.style.transition = "0.5s ease-in-out"
+        }
+        imagesContainer.style.transform = `translateX(-${width * counter}px)`
+        clearInterval(sliding)
+        sliding = setInterval(slide, 5000)
+    })
+}
